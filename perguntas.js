f42ee9c1edb7b84e55a44f24441d7aefe3dce964
beatrizsilvaheckmann <159556324+beatrizsilvaheckmann@users.noreply.github.com>
2024-09-12 08:48:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'matematica',
+    'Qual o valor de π?',
+    'Aproximadamente 3,14'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o rio mais extenso do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'biologia',
+    'O que é uma célula eucariótica?',
+    'é uma célula com nucleo definido e organelas menbranosas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
